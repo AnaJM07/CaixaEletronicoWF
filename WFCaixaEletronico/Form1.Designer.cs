98b043bab0cd7be3a5676a3cd2c24a51d218e5ec
AnaJM07 <anamartinis71@gmail.com>
2021-01-17 17:34:51 -0300

    CaixaEletronicoWindowsForms

---
+++ WFCaixaEletronico/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WFCaixaEletronico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNotas = new System.Windows.Forms.TextBox();
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.btnSacar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(164, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Saque Eletronico";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor do Saque:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Notas:";
+            // 
+            // txtNotas
+            // 
+            this.txtNotas.Enabled = false;
+            this.txtNotas.Location = new System.Drawing.Point(66, 178);
+            this.txtNotas.Multiline = true;
+            this.txtNotas.Name = "txtNotas";
+            this.txtNotas.Size = new System.Drawing.Size(151, 57);
+            this.txtNotas.TabIndex = 3;
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.Location = new System.Drawing.Point(66, 107);
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(100, 20);
+            this.txtValor1.TabIndex = 4;
+            this.txtValor1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtValor1_KeyPress);
+            // 
+            // btnSacar
+            // 
+            this.btnSacar.Location = new System.Drawing.Point(66, 277);
+            this.btnSacar.Name = "btnSacar";
+            this.btnSacar.Size = new System.Drawing.Size(75, 27);
+            this.btnSacar.TabIndex = 5;
+            this.btnSacar.Text = "Sacar";
+            this.btnSacar.UseVisualStyleBackColor = true;
+            this.btnSacar.Click += new System.EventHandler(this.btnSacar_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(202, 279);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Limpar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 353);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnSacar);
+            this.Controls.Add(this.txtValor1);
+            this.Controls.Add(this.txtNotas);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNotas;
+        private System.Windows.Forms.TextBox txtValor1;
+        private System.Windows.Forms.Button btnSacar;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WFCaixaEletronico/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFCaixaEletronico
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSacar_Click(object sender, EventArgs e)
+        {
+            bool isvalid = true;
+            int valor;
+            int c100 = 0;
+            int c50 = 0;
+            int c20 = 0;
+            int c10 = 0;
+
+            valor = String.IsNullOrEmpty(txtValor1.Text) ? 0 : Convert.ToInt32(txtValor1.Text);
+            if (valor == 0 )
+            {
+                MessageBox.Show("Valor Invalido");
+                isvalid = false;
+                return;
+            }
+            while (valor > 0)
+            {
+                if (valor >= 100)
+                {
+                    valor -= 100;
+                    c100++;
+                }
+                else if (valor >= 50)
+                {
+                    valor -= 50;
+                    c50++;
+                }
+                else if (valor >= 20)
+                {
+                    valor -= 20;
+                    c20++;
+                }
+                else if (valor >= 10)
+                {
+                    valor -= 10;
+                    c10++;
+                }
+                else
+                {
+                    MessageBox.Show("Valor Invalido");
+                    isvalid = false;
+                    break;
+                }
+            }
+            if (isvalid == true)
+            {
+                txtNotas.Text = $"Foram utilizadas: {c100} - Notas de 100," +
+                    $" {c50} - Notas de 50, {c20} " +
+                    $"- Notas de 20," +
+                    $" {c10} - Notas de 10";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtNotas.Clear();
+            txtValor1.Clear();
+        }
+
+        private void txtValor1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                //Atribui True no Handled para cancelar o evento
+                e.Handled = true;
+            }
+        }
+    }
+}
